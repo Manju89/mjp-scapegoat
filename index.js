52f9839162a63f4d62b3cf1b2580b448a14983c1
manjula <manjula@adroitlogic.com>
2018-01-11 23:34:06 +0530

    Refactoring

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-import SampleJS from "./SampleJS";
+let AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
 
 export const escape = (html) => {
     let sampleJS = new SampleJS();
     return sampleJS.customMethod(html);
 };
 
-const CustomAPI = {
-    escape
+export const escape2 = (html) => {
+    let sampleJS = new SampleJS();
+    return sampleJS.customMethod(html);
 };
 
-export default CustomAPI;
+const SLSQS = {
+    escape,
+    escape2
+};
+
+export default SLSQS;
