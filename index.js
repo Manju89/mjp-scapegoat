800f69ef85293a65c4e98ea95441b187dc53d029
manjula <manjula@adroitlogic.com>
2018-01-11 23:04:35 +0530

    Refactoring

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-export default {
-  escape: function(html) {
-    let sampleJS = new SampleJS();
-    return sampleJS.customMethod(html);
-  },
-
-  /**
-   * Unescape special characters in the given string of html.
-   *
-   * @param  {String} html
-   * @return {String}
-   */
-  unescape: function(html) {
-    return String(html)
-      .replace(/&amp;/g, '&')
-      .replace(/&quot;/g, '"')
-      .replace(/&#39;/g, '\'')
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>');
-  }
-};
-
 import SampleJS from "./SampleJS";
 
+export const escape = (html) => {
+    let sampleJS = new SampleJS();
+    return sampleJS.customMethod(html);
+};
+
+const CustomAPI = {
+    fetchUser
+};
+
+export default CustomAPI;
