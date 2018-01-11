04dc38ba6251164fb9e52ee6c8a1000a4cce71d3
manjula <manjula@adroitlogic.com>
2018-01-11 18:52:01 +0530

    Adding external file

---
+++ SampleJS.js
@@ -0,0 +1,8 @@
+function custom(input) {
+    return String(html)
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
